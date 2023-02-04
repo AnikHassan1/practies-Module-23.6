1fa8557c19a5cb926150f93ecc69559949959dcb
Anik Hassan1 <ah491606@gmail.com>
2023-02-04 22:42:35 +0600

    first commit

---
+++ list.js
@@ -0,0 +1,106 @@
+
+/*
+1. string , boolean , number....
+
+var name = 'anik';
+var names=45;
+var namas=true;
+console.log(typeof name);
+console.log(typeof namas)
+console.log( typeof names)
+*/
+
+
+/* 2. how does let and const uses for variable ?
+ let name='anik ';
+ const Name='sojib';
+ name='hassan';
+ console.log(name)
+ console.log(Name);
+*/
+
+/* 3. sum , multiple, addition
+var num1=50;
+var num2=40;
+var number=num1*num2;
+console.log(number);
+*/
+
+/* 4. Two Variable compiare
+
+let namo=60;
+let names=50;
+if(names == namo){
+    console.log('Soman Soman');
+}
+else{
+    console.log('not soman');
+}
+*/
+
+/* 5.6!!!! uses for && and || 
+
+let number=67;
+let num1=68;
+let num2 =9;
+if(number>num1 || number>num2){
+    console.log(number);
+}
+else if(num1>number && num1>num2){
+    console.log(num1);
+}
+else{
+    console.log(num2);
+}
+*/
+/*7. while loop 7 theke 9 odd shongha!!!
+for(let i=7;i<=19;i++){
+    if(i%2===1){
+        console.log("this odd number is :",i)
+    }
+}
+*/
+/* 8. array uses Array.Length, array,indexof Array,array value change,push.pop;
+let names=['anik', 'hassan','tanzim','sojib','Akash','Mahfuz']
+ names[3]='arafat';
+ names.push('Naim','Robin');
+ names.unshift('tarek');
+console.log(names)
+*/
+/*9.Array For Loop
+let names=['anik', 'hassan','tanzim','sojib','Akash','Mahfuz']
+for(let i=0;i<names.length;i++){
+    const index=i;
+    const element=names[index];
+    console.log(index,element);
+}
+*/
+/*
+10.Array of numbers > 80 !!!!
+let numbers = [56, 87, 45, 90, 21, 33, 88, 87]
+    for (let i = 0; i < numbers.length; i++) {
+        const element=numbers[i];
+        if (element >= 80) {
+          console.log(element);
+            
+        }
+    }
+    */
+/* 11. three Numbers Multiple!!!
+
+function multiple(num1,num2,num3){
+    let Number=num1*num2*num3;
+    return Number;
+}
+console.log(multiple(3,3,3));
+*/
+/*12.object property Change!!!
+let Names={
+    Name:'Anik Hassan',
+    Age:20,
+    Roll:477341,
+    department:'CSt',
+}
+Names.department='Ent';
+console.log(Names);
+*/
